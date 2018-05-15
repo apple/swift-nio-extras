7358cf1fa45b738064613a88b9f89d07093d28f2
Johannes Weiss <johannesweiss@apple.com>
2018-05-15 15:35:03 +0100

    quiescing helper & demo
    Motivation:
    
    In a few cases quiescing a server application is useful but it's harder
    than necessary with core-NIO. Therefore this adds a helper & a
    demonstration.
    
    Modifications:
    
    - add `QuiescingHelper` which helps users to quiesce a channel by
      collecting all accepted channels and when needed sends them the
      quiescing user event. When all collected channels have closed the
      user will be notified and can just shut down the ELG.
    - added a demo implementation with a simple HTTP server that quiesces
      when receiving a signal
    
    Result:
    
    Make it quite easy to quiesce a server and show users how to do it.

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:4.0
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2017-2018 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import PackageDescription
+
+var targets: [PackageDescription.Target] = [
+    .target(name: "NIOExtras", dependencies: ["NIO"]),
+    .target(name: "HTTPServerWithQuiescingDemo", dependencies: ["NIOExtras", "NIOHTTP1"]),
+    .testTarget(name: "NIOExtrasTests", dependencies: ["NIOExtras"]),
+]
+
+let package = Package(
+    name: "swift-nio-extras",
+    products: [
+        .executable(name: "HTTPServerWithQuiescingDemo", targets: ["HTTPServerWithQuiescingDemo"]),
+        .library(name: "NIOExtras", targets: ["NIOExtras"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-nio.git", from: "1.7.0"),
+    ],
+    targets: targets
+)
