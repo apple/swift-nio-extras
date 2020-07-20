319208c01d4468305fcd6d79f3d5858eeae8bde2
Peter Adams <pp_adams@apple.com>
2020-07-20 09:56:29 +0100

    Checkout before George suggested changes.

--- Sources/NIOExtras/PCAPRingBuffer.swift
+++ Sources/NIOExtras/PCAPRingBuffer.swift
@@ -15,7 +15,7 @@
 import NIO
 
 /// Storage for the most recent set of packets captured subject to constraints.
-internal struct PCAPRingBuffer {
+public struct PCAPRingBuffer {
     private var pcapFragments: CircularBuffer<ByteBuffer>
     private var pcapCurrentBytes: size_t
     private let maximumFragments: UInt
@@ -25,7 +25,7 @@ internal struct PCAPRingBuffer {
     /// Parameters:
     ///    - maximumFragments: The maximum number of pcap fragments to store.
     ///    - maximumBytes:  The maximum number of bytes to store - note, data written may exceed this by the header size.
-    init(maximumFragments: UInt, maximumBytes: size_t) {
+    public init(maximumFragments: UInt, maximumBytes: size_t) {
         self.maximumFragments = maximumFragments
         self.maximumBytes = maximumBytes
         self.pcapCurrentBytes = 0
@@ -47,7 +47,9 @@ internal struct PCAPRingBuffer {
     }
 
     /// Record a fragment into the buffer, making space if required.
-    mutating func addFragment(_ buffer: ByteBuffer) {
+    /// Parameters:
+    /// - buffer: ByteBuffer containing a pcap fragment to store.
+    public mutating func addFragment(_ buffer: ByteBuffer) {
         // Make sure we don't go over on the number of fragments.
         if self.pcapFragments.count >= self.maximumFragments {
             self.popFirst()
@@ -64,11 +66,73 @@ internal struct PCAPRingBuffer {
     }
 
     /// Emit the captured data to a byteBuffer - this drains the captured data.
-    mutating func emitPCAP(allocator: ByteBufferAllocator) -> ByteBuffer {
+    /// Parameters:
+    /// - allocator: Allocator for creating byte buffers which are stored in the ring buffer.
+    public mutating func emitPCAP(allocator: ByteBufferAllocator) -> ByteBuffer {
         var buffer = allocator.buffer(capacity: self.pcapCurrentBytes)
         while var next = self.popFirst() {
             buffer.writeBuffer(&next)
         }
         return buffer
+    }
+}
+
+/// Handler to pair with `NIOWritePCAPHandler` to  capture a set of packets prior to an user triggered event.
+/// Send `PCAPRingCaptureHandler.RecordPreviousPackets` through as either an InboundEvent or OutboudEvent as
+/// appropriate to trigger recording through the `sink` initialisation parameter.
+public class PCAPRingCaptureHandler: ChannelDuplexHandler, RemovableChannelHandler {
+    public typealias InboundIn = Any // Don't care, not looking
+    public typealias OutboundIn = Any // Don't care, not looking
+
+    private var pcapBuffer: PCAPRingBuffer
+    private let sink: (NIO.ByteBuffer) -> Void
+
+    /// Initialise.
+    /// Parameters:
+    /// - maximumFragments: Maximum number of fragments to record in any capture.
+    /// - maximumBytes: Maximum number of bytes to record in any capture.
+    /// - sink: Where to send captured data to.
+    public init(maximumFragments: UInt, maximumBytes: size_t, sink: @escaping (NIO.ByteBuffer) -> Void) {
+        self.pcapBuffer = PCAPRingBuffer(maximumFragments: maximumFragments, maximumBytes: maximumBytes)
+        self.sink = sink
+    }
+
+    /// Triggers writing captured data to the sink if `RecordPreviousPackets` is seen.
+    public func userInboundEventTriggered(context: ChannelHandlerContext, event: Any) {
+        if let request = event as? RecordPreviousPackets {
+            recordPCAP(allocator: context.channel.allocator)
+        } else {
+            context.fireUserInboundEventTriggered(event)
+        }
+    }
+
+    /// Triggers writing captured data to the sink if `RecordPreviousPackets` is seen.
+    public func triggerUserOutboundEvent(context: ChannelHandlerContext, event: Any, promise: EventLoopPromise<Void>?) {
+        if let request = event as? RecordPreviousPackets {
+            recordPCAP(allocator: context.channel.allocator)
+            promise?.succeed(())
+        } else {
+            context.triggerUserOutboundEvent(event, promise: promise)
+        }
+    }
+
+    private func recordPCAP(allocator: ByteBufferAllocator) {
+        // Grab the data - and send it to the sink.
+        let capturedData = self.pcapBuffer.emitPCAP(allocator: allocator)
+        sink(capturedData)
+    }
+
+    /// Record a fragment into the buffer, making space if required.
+    /// This should almost certainly be connected as the sink for a `WritePCAPHandler`
+    /// Parameters:
+    /// - buffer: Captured fragment to record.
+    public func addFragment(_ buffer: NIO.ByteBuffer) {
+        self.pcapBuffer.addFragment(buffer)
+    }
+}
+
+extension PCAPRingCaptureHandler {
+    public struct RecordPreviousPackets {
+        public init() { }
     }
 }
---
+++ Sources/NIOWritePartialPCAPDemo/main.swift
@@ -0,0 +1,127 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2019 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import NIO
+import NIOExtras
+import NIOHTTP1
+
+
+
+class TriggerPCAPHandler: ChannelInboundHandler {
+    typealias InboundIn = HTTPClientResponsePart
+    typealias OutboundOut = HTTPClientRequestPart
+
+    func channelRead(context: ChannelHandlerContext, data: NIOAny) {
+        if case .head(let header) = self.unwrapInboundIn(data) {
+            if header.status == .preconditionFailed {
+                // For the sake of a repeatable demo, let's assume that seeing a preconditionFailed
+                // status is the sign that the issue you're looking to diagnose has happened.
+                context.triggerUserOutboundEvent(PCAPRingCaptureHandler.RecordPreviousPackets(),
+                                                 promise: nil)
+            }
+        }
+        context.fireChannelRead(data)
+    }
+}
+
+class SendSimpleSequenceRequestHandler: ChannelInboundHandler {
+    typealias InboundIn = HTTPClientResponsePart
+    typealias OutboundOut = HTTPClientRequestPart
+    
+    private let allDonePromise: EventLoopPromise<Void>
+
+    private var nextReqeustNumber = 0
+    private var requestsToMake: [HTTPResponseStatus] = [ .ok, .created, .accepted, .nonAuthoritativeInformation,
+                                                         .noContent, .resetContent, .preconditionFailed,
+                                                         .partialContent, .multiStatus, .alreadyReported ]
+
+    init(allDonePromise: EventLoopPromise<Void>) {
+        self.allDonePromise = allDonePromise
+    }
+    
+    func channelRead(context: ChannelHandlerContext, data: NIOAny) {
+        if case .end = self.unwrapInboundIn(data) {
+            makeNextReqeustOrComplete(context: context)
+        }
+    }
+    
+    func errorCaught(context: ChannelHandlerContext, error: Error) {
+        self.allDonePromise.fail(error)
+        context.close(promise: nil)
+    }
+    
+    func channelActive(context: ChannelHandlerContext) {
+        makeNextReqeustOrComplete(context: context)
+    }
+
+    private func makeNextReqeustOrComplete(context: ChannelHandlerContext) {
+        if self.nextReqeustNumber < self.requestsToMake.count {
+            let headers = HTTPHeaders([("host", "httpbin.org"),
+                                       ("accept", "application/json")])
+            let currentStatus = self.requestsToMake[self.nextReqeustNumber].code
+            self.nextReqeustNumber += 1
+            context.write(self.wrapOutboundOut(.head(.init(version: .init(major: 1, minor: 1),
+                                                           method: .GET,
+                                                           uri: "/status/\(currentStatus)",
+                                                           headers: headers))), promise: nil)
+            context.writeAndFlush(self.wrapOutboundOut(.end(nil)), promise: nil)
+        } else {
+            self.allDonePromise.succeed(())
+        }
+    }
+}
+
+guard let outputFile = CommandLine.arguments.dropFirst().first else {
+    print("Usage: \(CommandLine.arguments[0]) OUTPUT.pcap")
+    exit(0)
+}
+
+let fileSink = try NIOWritePCAPHandler.SynchronizedFileSink.fileSinkWritingToFile(path: outputFile) { error in
+    print("ERROR: \(error)")
+    exit(1)
+}
+
+let group = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount)
+defer {
+    try! group.syncShutdownGracefully()
+}
+let allDonePromise = group.next().makePromise(of: Void.self)
+let connection = try ClientBootstrap(group: group.next())
+    .channelInitializer { channel in
+        let ringCaptureHandler = PCAPRingCaptureHandler(maximumFragments: 4,
+                                                        maximumBytes: 1_000_000,
+                                                        sink: fileSink.write)
+        return channel.pipeline.addHandler(NIOWritePCAPHandler(
+                                            mode: .client,
+                                            fileSink: { ringCaptureHandler.addFragment($0) })).flatMap {
+            channel.pipeline.addHTTPClientHandlers()
+        }.flatMap {
+            channel.pipeline.addHandler(ringCaptureHandler)
+        }.flatMap {
+            channel.pipeline.addHandler(TriggerPCAPHandler())
+        }.flatMap {
+            channel.pipeline.addHandler(SendSimpleSequenceRequestHandler(allDonePromise: allDonePromise))
+        }
+    }
+    .connect(host: "httpbin.org", port: 80)
+    .wait()
+try allDonePromise.futureResult.wait()
+print("# Success!")
+try connection.close().wait()
+try fileSink.syncClose()
+print("# Your pcap file should have been written to '\(outputFile)'")
+print("#")
+print("# You can view \(outputFile) with")
+print("# - Wireshark")
+print("# - tcpdump -r '\(outputFile)'")
