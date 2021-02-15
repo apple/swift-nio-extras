809d65ece10bf5415a3804dedc89d8adc0a3f906
David Nadoba <dnadoba@gmail.com>
2021-02-15 09:48:04 +0100

    prepare LengthFieldPrepender.BitLength for reuse

---
+++ Sources/NIOExtras/LengthFieldBitLength.swift
@@ -0,0 +1,69 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftNIO open source project
+//
+// Copyright (c) 2017-2021 Apple Inc. and the SwiftNIO project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftNIO project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+
+/// An struct to describe the length of a piece of data in bits
+public struct LengthFieldBitLength {
+    internal enum Backing {
+        case bits8
+        case bits16
+        case bits24
+        case bits32
+        case bits64
+    }
+    internal let bitLength: Backing
+
+    public static let oneByte = LengthFieldBitLength(bitLength: .bits8)
+    public static let twoBytes = LengthFieldBitLength(bitLength: .bits16)
+    public static let threeBytes = LengthFieldBitLength(bitLength: .bits24)
+    public static let fourBytes = LengthFieldBitLength(bitLength: .bits32)
+    public static let eightBytes = LengthFieldBitLength(bitLength: .bits64)
+    
+    public static let eightBits = LengthFieldBitLength(bitLength: .bits8)
+    public static let sixteenBits = LengthFieldBitLength(bitLength: .bits16)
+    public static let twentyFourBits = LengthFieldBitLength(bitLength: .bits24)
+    public static let thirtyTwoBits = LengthFieldBitLength(bitLength: .bits32)
+    public static let sixtyFourBits = LengthFieldBitLength(bitLength: .bits64)
+    
+    internal var length: Int {
+        switch bitLength {
+        case .bits8:
+            return 1
+        case .bits16:
+            return 2
+        case .bits24:
+            return 3
+        case .bits32:
+            return 4
+        case .bits64:
+            return 8
+        }
+    }
+    
+    internal var max: UInt {
+        switch bitLength {
+        case .bits8:
+            return UInt(UInt8.max)
+        case .bits16:
+            return UInt(UInt16.max)
+        case .bits24:
+            return (UInt(UInt16.max) << 8) &+ UInt(UInt8.max)
+        case .bits32:
+            return UInt(UInt32.max)
+        case .bits64:
+            return UInt(UInt64.max)
+        }
+    }
+}
--- Sources/NIOExtras/LengthFieldPrepender.swift
+++ Sources/NIOExtras/LengthFieldPrepender.swift
@@ -53,60 +53,6 @@ public enum LengthFieldPrependerError: Error {
 ///
 public final class LengthFieldPrepender: ChannelOutboundHandler {
     ///
-    /// An struct to describe the length of a piece of data in bits
-    public struct BitLength {
-        fileprivate enum Backing {
-            case bits8
-            case bits16
-            case bits24
-            case bits32
-            case bits64
-        }
-        fileprivate let bitLength: Backing
-
-        public static let oneByte: BitLength = BitLength(bitLength: .bits8)
-        public static let twoBytes: BitLength = BitLength(bitLength: .bits16)
-        public static let threeBytes: BitLength = BitLength(bitLength: .bits24)
-        public static let fourBytes: BitLength = BitLength(bitLength: .bits32)
-        public static let eightBytes: BitLength = BitLength(bitLength: .bits64)
-        
-        public static let eightBits: BitLength = BitLength(bitLength: .bits8)
-        public static let sixteenBits: BitLength = BitLength(bitLength: .bits16)
-        public static let twentyFourBits: BitLength = BitLength(bitLength: .bits24)
-        public static let thirtyTwoBits: BitLength = BitLength(bitLength: .bits32)
-        public static let sixtyFourBits: BitLength = BitLength(bitLength: .bits64)
-        
-        fileprivate var length: Int {
-            switch bitLength {
-            case .bits8:
-                return 1
-            case .bits16:
-                return 2
-            case .bits24:
-                return 3
-            case .bits32:
-                return 4
-            case .bits64:
-                return 8
-            }
-        }
-        
-        fileprivate var max: UInt {
-            switch bitLength {
-            case .bits8:
-                return UInt(UInt8.max)
-            case .bits16:
-                return UInt(UInt16.max)
-            case .bits24:
-                return (UInt(UInt16.max) << 8) &+ UInt(UInt8.max)
-            case .bits32:
-                return UInt(UInt32.max)
-            case .bits64:
-                return UInt(UInt64.max)
-            }
-        }
-    }
-    ///
     /// An enumeration to describe the length of a piece of data in bytes.
     ///
     public enum ByteLength {
@@ -115,7 +61,7 @@ public final class LengthFieldPrepender: ChannelOutboundHandler {
         case four
         case eight
         
-        fileprivate var bitLength: BitLength {
+        fileprivate var bitLength: LengthFieldBitLength {
             switch self {
             case .one: return .oneByte
             case .two: return .twoBytes
@@ -128,7 +74,7 @@ public final class LengthFieldPrepender: ChannelOutboundHandler {
     public typealias OutboundIn = ByteBuffer
     public typealias OutboundOut = ByteBuffer
 
-    private let lengthFieldLength: LengthFieldPrepender.BitLength
+    private let lengthFieldLength: LengthFieldBitLength
     private let lengthFieldEndianness: Endianness
     
     private var lengthBuffer: ByteBuffer?
@@ -142,7 +88,7 @@ public final class LengthFieldPrepender: ChannelOutboundHandler {
     public convenience init(lengthFieldLength: ByteLength, lengthFieldEndianness: Endianness = .big) {
         self.init(lengthFieldBitLength: lengthFieldLength.bitLength, lengthFieldEndianness: lengthFieldEndianness)
     }
-    public init(lengthFieldBitLength: BitLength, lengthFieldEndianness: Endianness = .big) {
+    public init(lengthFieldBitLength: LengthFieldBitLength, lengthFieldEndianness: Endianness = .big) {
         // The value contained in the length field must be able to be represented by an integer type on the platform.
         // ie. .eight == 64bit which would not fit into the Int type on a 32bit platform.
         precondition(lengthFieldBitLength.length <= Int.bitWidth/8)
