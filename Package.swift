927ae8ce7bfe1b0157404abdf25b2c81c9432fab
Eric Rosenberg <eric_rosenberg@apple.com>
2025-01-21 16:35:31 +0000

    drop unneeded swift-argument-parser

--- Package.swift
+++ Package.swift
@@ -242,7 +242,6 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-http-types.git", from: "1.3.0"),
         .package(url: "https://github.com/apple/swift-http-structured-headers.git", from: "1.1.0"),
         .package(url: "https://github.com/apple/swift-atomics.git", from: "1.2.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.4.0"),
         .package(url: "https://github.com/apple/swift-algorithms.git", from: "1.2.0"),
 
     ],
